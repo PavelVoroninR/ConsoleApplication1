22a84f2db67bace561b531c234ab3e09c99b3d67
voroninpr <palik666@gmail.com>
2017-09-11 14:58:17 +0300

    some fix

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -36,8 +36,8 @@ namespace ConsoleApplication1
 			}
 			catch (Exception e)
 			{
-				Console.WriteLine("Error {0}", e);
-				exPath = curDir;
+                //Console.WriteLine("Error {0}", e);
+                exPath = curDir;
 				Console.WriteLine("Used Path {0}", curDir);
 			}
 			getConfig cfg = new getConfig();
@@ -75,18 +75,29 @@ namespace ConsoleApplication1
 	}
 	class getConfig
 	{
+		// отдельный файл
 		private List<string> exFilename { get; }
+		// ???
 		private List<bool> exFull { get; }
+		// Читать все файлы в каталоге
 		private bool exAllFileInDir { get; }
+		// перевый символ в сетке excel
 		private List<string> exFirstLetter { get; }
+		// последний символ в сетке excel
 		private List<string> exLastLetter { get; }
+		// первая цифира в сетке excel
 		private List<int> exFirstCell { get; }
+		// последняя цифира в сетке excel
 		private List<int> exLastCell { get; }
 
+		//индивидуальные настройки для каждого файла CSV
 		private bool indivCsvSettings { get; }
+		//индивидуальные настройки для файлов excel
 		private bool indivXlsxSettings { get; }
 
+		//разделитель записей в CSV файле
 		private List<char> csvDelimetr { get; }
+		//десятичный разделитель в CSV файле
 		private List<char> csvDecDelimetr { get; }
 
 		public getConfig()
@@ -94,6 +105,43 @@ namespace ConsoleApplication1
 			string defPath = (Environment.CurrentDirectory+@"\setings.cfg");
 			try
 			{   // Open the text file using a stream reader.
+				if (!File.Exists(defPath)) {
+					File.Create(defPath);
+					using (StreamWriter sw = new StreamWriter(defPath)) {
+						string cfgString = "indivXlsxSettings = false\n" +
+							"indivCsvSettings = false\n" +
+							"##Global parametrs:\n" +
+							"exFirstLetter = A\n" +
+							"exLastLetter = I\n" +
+							"exFirstCell = 1\n" +
+							"exLastCell = 0\n" +
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					}
+				}
 				using (StreamReader sr = new StreamReader(defPath))
 				{
 					// Read the stream to a string, and write the string to the console.
