7d67dd5b67cac68e10f2e9c8da4392b02446ffcc
voroninpr <palik666@gmail.com>
2017-09-19 13:41:21 +0300

    Added class 4 work with ini files

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Packaging;
-using System.IO;
+
 using System.Xml;
+
+
+
+
 
 namespace ConsoleApplication1
 {
@@ -40,10 +49,12 @@ namespace ConsoleApplication1
                 exPath = curDir;
 				Console.WriteLine("Used Path {0}", curDir);
 			}
-			getConfig cfg = new getConfig();
+			//getConfig cfg = new getConfig();
 			Console.ReadKey();
 		}
 	}
+
+
 	class workCode
 	{
 		private List<string> exPath { get; set; }
@@ -73,7 +84,9 @@ namespace ConsoleApplication1
 		}
 
 	}
-	class getConfig
+
+
+	/*class getConfig
 	{
 		// отдельный файл
 		private List<string> exFilename { get; }
@@ -99,6 +112,8 @@ namespace ConsoleApplication1
 		private List<char> csvDelimetr { get; }
 		//десятичный разделитель в CSV файле
 		private List<char> csvDecDelimetr { get; }
+
+
 
 		public getConfig()
 		{
@@ -168,6 +183,55 @@ namespace ConsoleApplication1
 				Console.WriteLine("The file could not be read:");
 				Console.WriteLine(e.Message);
 			}
+		}
+	}*/
+
+
+	/*using System.IO;
+	using System.Text;
+	using System.Reflection;
+	using System.Runtime.InteropServices;*/
+	class IniFile   // revision 11
+	{
+		string Path;
+		string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+		[DllImport("kernel32", CharSet = CharSet.Unicode)]
+		static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+		[DllImport("kernel32", CharSet = CharSet.Unicode)]
+		static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+		public IniFile(string IniPath = null)
+		{
+			Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+		}
+
+		public string Read(string Key, string Section = null)
+		{
+			var RetVal = new StringBuilder(255);
+			GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+			return RetVal.ToString();
+		}
+
+		public void Write(string Key, string Value, string Section = null)
+		{
+			WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+		}
+
+		public void DeleteKey(string Key, string Section = null)
+		{
+			Write(Key, null, Section ?? EXE);
+		}
+
+		public void DeleteSection(string Section = null)
+		{
+			Write(null, null, Section ?? EXE);
+		}
+
+		public bool KeyExists(string Key, string Section = null)
+		{
+			return Read(Key, Section).Length > 0;
 		}
 	}
 }
