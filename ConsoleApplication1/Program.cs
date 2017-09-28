1e98901cc0b5401dcb94ef7a163de4c94e1afaf1
voroninpr <palik666@gmail.com>
2017-09-28 11:32:41 +0300

    Drop out some useless code, add some meembers from class "WorkCode"

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -57,134 +57,33 @@ namespace ConsoleApplication1
 
 	class workCode
 	{
+
 		private List<string> exPath { get; set; }
 		private List<string> bufCellVal = new List<string>();
 		private List<SpreadsheetDocument> exBook;
 		private bool exEdit { get; set; }
 		private bool endDocStream {get; }
 
-		public workCode()
-		{
-			string defPath = Environment.CurrentDirectory;
-			var tmpVar = Directory.GetFiles(defPath, @"*.xlsx");
-			foreach (var tmpFname in tmpVar)
-			{
-				exPath.Add(tmpVar.ToString());
+		public bool checkIni(string Path) {
+			if (File.Exists(Path)) {
+				return true;
+			} else {
+				return false;
 			}
-			exEdit = false;
-
+			
 		}
 
-		public void getExBookCells()
+		public void createIni(string Path)
 		{
-			foreach (string exExl in exPath)
-			{
-				exBook.Add(SpreadsheetDocument.Open(exExl, false/*exEdit*/));
-			}		
+			File.Create(Path);
+
+
+
+			System.Windows.Forms.MessageBox.Show("Can't find ini file! This File was created automaticly");
 		}
 
-	}
 
 
-	/*class getConfig
-	{
-		// отдельный файл
-		private List<string> exFilename { get; }
-		// Читать файл целиком
-		private List<bool> exFull { get; }
-		// Читать все файлы в каталоге
-		private bool exAllFileInDir { get; }
-		// перевый символ в сетке excel
-		private List<string> exFirstLetter { get; }
-		// последний символ в сетке excel
-		private List<string> exLastLetter { get; }
-		// первая цифира в сетке excel
-		private List<int> exFirstCell { get; }
-		// последняя цифира в сетке excel
-		private List<int> exLastCell { get; }
-
-		//индивидуальные настройки для каждого файла CSV
-		private bool indivCsvSettings { get; }
-		//индивидуальные настройки для файлов excel
-		private bool indivXlsxSettings { get; }
-
-		//разделитель записей в CSV файле
-		private List<char> csvDelimetr { get; }
-		//десятичный разделитель в CSV файле
-		private List<char> csvDecDelimetr { get; }
-
-
-
-		public getConfig()
-		{
-			string defPath = (Environment.CurrentDirectory+@"\setings.cfg");
-			try
-			{   // Open the text file using a stream reader.
-				if (!File.Exists(defPath)) {
-					File.Create(defPath);
-					using (StreamWriter sw = new StreamWriter(defPath)) {
-						string cfgString = "#This file generated automaticly. Use \"true\" and \n"+
-							"\"false\" for set boolean and letters and number for other. Use 0 for mark infinity" + 
-							"indivXlsxSettings = false\n" +
-							"indivCsvSettings = false\n" +
-							"#Global parametrs:\n" +
-							"exFull = false\n" + 
-							"exFirstLetter = A\n" +
-							"exLastLetter = I\n" +
-							"exFirstCell = 1\n" +
-							"exLastCell = 0\n" +
-							"csvDelimetr = |" +
-							"csvDecDelimetr = ." +
-							"#Indial settings:" +
-							"exFilename = " +
-							"exFull = true" +
-							"exFirstLetter = A\n" +
-							"exLastLetter = I\n" +
-							"exFirstCell = 1\n" +
-							"exLastCell = 0\n" +
-							"#Indial settings:" +
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-					}
-				}
-				using (StreamReader sr = new StreamReader(defPath))
-				{
-					// Read the stream to a string, and write the string to the console.
-					String line = sr.ReadToEnd();
-					Console.WriteLine(line);
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("The file could not be read:");
-				Console.WriteLine(e.Message);
-			}
-		}
-	}*/
 
 
 	/*using System.IO;
